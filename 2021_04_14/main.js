44b678b06230f8d83987d7d8b2947f7e9b434a95
potrus.m21 <potrus.m21@cf.dnu.dp.ua>
2021-04-15 14:49:48 +0300

    doc: first lesson

---
+++ 2021_04_14/main.js
@@ -0,0 +1,24 @@
+var isAgree = confirm("This program calculates the minimum number of paint cans needed to \npaint walls in an office. Do you want to continue?");
+if (isAgree) {
+    let L = prompt("Enter L:", '');
+    let W = prompt("Enter W:", '');
+    let H = prompt("Enter H:", '');
+    let inputData = [L, W, H];
+    for (let item = 0; item < 3; item++) {
+        while (inputData[item] >= 1000) {
+            switch (item) {
+                case 0:
+                    inputData[item] = prompt("L must be less than 1000, enter again:", '');
+                    break;
+                case 1:
+                    inputData[item] = prompt("W must be less than 1000, enter again:", '');
+                    break;
+                case 2:
+                    inputData[item] = prompt("H must be less than 1000, enter again:", '');
+                    break;
+            }
+        }
+    }
+    /*функция для расчета боковой площади прямоугольного параллелепипеда равна S = 2*c*(a+b)*/
+    alert(2 * Number(H) * (Number(L) + Number(W)) / 16);
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-### This repository stores tasks and their solutions in accordance with the dates
+## This repository stores tasks and their solutions in accordance with the dates:
+#
+### 2021.04.14
+- Task 1: Repairs
+- Task 2: Golden sand
+- Task 3: Lucky ticket
