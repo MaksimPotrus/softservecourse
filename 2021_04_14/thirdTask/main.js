b8c6ee51a204e7df288d39ef5022c7f27d913eda
potrus.m21 <potrus.m21@cf.dnu.dp.ua>
2021-04-16 18:26:36 +0300

    doc: refactor third task

--- 2021_04_14/thirdTask/main.js
+++ 2021_04_14/thirdTask/main.js
@@ -1,6 +1,9 @@
 var isAgree = confirm("This program checks the ticket's luck. Do you want to continue?");
 if (isAgree) {
   let ticket = prompt("Enter your ticket:", '');
+  while (ticket >= 999999 || ticket <= 0) {
+    ticket = +prompt("Enter current ticket:", '');
+  };
   let array = ([].map.call(ticket, function (x) { return x; }));
   let sumLeft = 0, sumRight = 0;
   for (let item = 0; item < array.length; item++) {
